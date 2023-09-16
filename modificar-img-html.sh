d94998af603d7d45a4b082386b3fe6f3cb6408ac
NEBULA1978 <rpascualey@gmail.com>
2023-09-16 11:58:16 +0200

    Prueba 7 ver lineas con nombre de imagenes dentro de  archivo html con patron

--- modificar-img-html.sh
+++ modificar-img-html.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 
-# Arreglo con las líneas a buscar
-lineas_a_buscar=(
-  "<img src=\"./img/camisetaBlanca.png\" alt=\"Camiseta Blanca\">"
-  "<img src=\"./img/camisetaNegra.png\" alt=\"Camiseta Negra\">"
-  "<img src=\"./img/corazon.png\" alt=\"Corazón\">"
-  "<img src=\"./img/rey_atanagildo.png\" alt=\"Rey Atanagildo\">"
-  "<img src=\"./img/rey_ataulfo.png\" alt=\"Rey Ataulfo\">"
-  "<img src=\"./img/rey_ervigio.png\" alt=\"Rey Ervigio\">"
-  "<img src=\"./img/rey_leogivildo.png\" alt=\"Rey Leovigildo\">"
-  "<img src=\"./img/rey_recesvinto.png\" alt=\"Rey Recesvinto\">"
-  "<img src=\"./img/rey_sisebuto.png\" alt=\"Rey Sisebuto\">"
-  "<img src=\"./img/rey_teodorico.png\" alt=\"Rey Teodorico\">"
-)
+# Patrón para buscar etiquetas <img>
+patron_img="<img .*>"
 
 # Nombre del archivo HTML a analizar
 archivo_html="index-img.html"
 
-# Recorremos el arreglo de líneas a buscar
-for linea in "${lineas_a_buscar[@]}"; do
+# Buscamos todas las líneas que contienen etiquetas <img> en el archivo HTML
+lineas_encontradas=$(grep -n "$patron_img" "$archivo_html")
 
-  # Buscamos la línea en el archivo HTML
-  linea_encontrada=$(grep -n "$linea" "$archivo_html")
-
-  # Imprimimos el número de línea donde se encontró la línea
-  echo "$linea_encontrada"
-
-done
+# Imprimimos las líneas encontradas
+echo "$lineas_encontradas"
