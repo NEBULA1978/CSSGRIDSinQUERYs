883591b55735d6cc92c9553de1515e6db89835d3
NEBULA1978 <rpascualey@gmail.com>
2023-09-16 12:13:26 +0200

    Prueba cambia con nombre de imagenes dentro de  archivo html con patron

--- modificar-img-html.sh
+++ modificar-img-html.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# Patrón para buscar etiquetas <img>
-patron_img="<img .*>"
-
 # Nombre del archivo HTML a analizar
 archivo_html="index-img.html"
 
-# Buscamos todas las líneas que contienen etiquetas <img> en el archivo HTML
-lineas_encontradas=$(grep -n "$patron_img" "$archivo_html")
+# Patrón de búsqueda y reemplazo
+patron_busqueda="./img/camisetaBlanca.png"
+patron_reemplazo="./img/camisetaNegra.png"
 
-# Imprimimos las líneas encontradas
-echo "$lineas_encontradas"
+# Realizar el reemplazo en el archivo HTML y guardar la salida en un archivo temporal
+sed "s|$patron_busqueda|$patron_reemplazo|g" "$archivo_html" > temporal.html
+
+# Mostrar las líneas modificadas y los nombres
+echo "Líneas modificadas:"
+grep -n "$patron_reemplazo" temporal.html
+
+# Preguntar al usuario si desea guardar los cambios en el archivo original
+echo "¿Deseas guardar los cambios en el archivo original? (Sí/No)"
+read -r respuesta
+
+if [[ "$respuesta" =~ ^[Ss][Ii]$ ]]; then
+  mv temporal.html "$archivo_html"
+  echo "Cambios guardados en el archivo original."
+else
+  rm temporal.html
+  echo "Cambios descartados."
+fi
